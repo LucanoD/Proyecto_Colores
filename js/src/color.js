25a7f0359493d63b7c85e186a125eb1e0ab3e7ab
Dani_Lucano <olucano10@gmail.com>
2021-07-16 02:25:09 -0500

    Proyecto Colores

---
+++ js/src/color.js
@@ -0,0 +1,7 @@
+color.value = storage;
+
+getColors();
+setColor();
+
+
+color.addEventListener("input", setColor);
---
+++ js/src/setColor.js
@@ -0,0 +1,7 @@
+const setColor = () => {
+    const value = color.value;
+
+    salida.innerHTML = value;
+    salida.style.background = value;
+
+};
